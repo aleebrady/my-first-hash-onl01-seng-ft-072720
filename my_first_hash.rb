b575166ab3a5161f31a3ef8b2b59a15ce226fd79
Aaron Brady <aleebrady@gmail.com>
2020-08-01 01:45:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,9 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale_bones_corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale_bones_corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3}
 end
 
 def retrieval
